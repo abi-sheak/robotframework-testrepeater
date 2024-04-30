876d1576bdbbaf2feebc72c86c97ecd5f3d2cdac
abi-sheak <abisheakk2@gmail.com>
2024-04-30 11:22:23 +0530

    Added Library for repeat test

---
+++ TestRepeater.py
@@ -0,0 +1,30 @@
+from robot.api import SuiteVisitor
+
+class TestRepeater(SuiteVisitor):
+    ROBOT_LISTENER_API_VERSION = 3
+
+    def __init__(self, count):
+        self.count = int(count)
+
+    def start_suite(self, suite, result):
+        testcases = []
+        for iter in range(1, self.count+1):
+            for test in suite.tests:
+                copy = test.copy(name=f"{iter} - {test.name}")
+                testcases.a1ppend(copy)
+        suite.tests = testcases
+
+    def end_suite(self, suite, result):
+        pass
+
+    def start_test(self, test, result):
+        pass
+
+    def end_test(self, test, result):
+        pass
+
+    def start_keyword(self,test, result):
+        pass
+
+    def end_keyword(self,test, result):
+        pass
